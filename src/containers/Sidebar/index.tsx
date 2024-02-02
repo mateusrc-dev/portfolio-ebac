00c09a614e96c5f99cb9909fb52110462be2c8be
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-02-02 16:44:18 -0300

    updated project

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/mateusrc-dev.png" alt="" />
+    <Titulo>Mateus Raimundo</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,18 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam
+      ut tempora nam hic aut alias earum repellendus voluptate optio ipsum
+      tempore, expedita, esse dolorem incidunt! Sunt aperiam nisi quidem? Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Placeat aliquid sequi
+      cumque cum neque aspernatur blanditiis, quasi veritatis, consectetur
+      corporis beatae sapiente dolor similique totam labore explicabo quaerat
+      nostrum dolores?
+    </p>
+  </section>
+)
+
+export default Sobre
