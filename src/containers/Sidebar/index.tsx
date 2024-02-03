e318a26c302bdbc33e62ee5aca33e4f89239ecd3
Mateus Carvalho <mateus.raimundo1995@gmail.com>
2024-02-03 11:03:19 -0300

    updated Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/mateusrc-dev.png" alt="" />
-    <Titulo fontSize={20}>Mateus Raimundo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Mateus Raimundo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        mateusrc-dev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Fullstack JAVA
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
